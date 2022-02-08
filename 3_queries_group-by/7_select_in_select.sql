af35bfbb7f6de5be44926f04d5f56b6c80ff904f
Edwin Ly <edwinjly@gmail.com>
2022-02-08 17:23:41 -0500

    Add teachers and assistance_request tables

---
+++ 3_queries_group-by/7_select_in_select.sql
@@ -0,0 +1,27 @@
+SELECT assignments.name
+FROM assignments 
+WHERE id NOT IN
+(
+  SELECT assignment_id
+  FROM assignment_submissions
+  JOIN students ON students.id = student_id
+  WHERE students.name = 'Ibrahim Schimmel'
+);
+
+-- SELECT avg(total_students) as average_students
+-- FROM (
+--   SELECT count(students) as total_students, cohorts.name as cohort_name
+--   FROM students
+--   JOIN cohorts on cohorts.id = cohort_id
+--   GROUP BY cohorts.name
+-- ) as totals_table;
+
+
+
+-- SELECT (
+--   SELECT count(assignments)
+--   FROM assignments
+-- )-count(assignment_submissions) as total_incomplete
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.name = 'Ibrahim Schimmel';
